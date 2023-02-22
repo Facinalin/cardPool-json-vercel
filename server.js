87a54ffe74941e9fb0ac520807bdd2299d6657f6
facina <lovemistborn13@gmail.com>
2023-02-22 17:29:34 +0800

    0222

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const auth = require("json-server-auth");
 const db = require("./db.json");
 const router = jsonServer.router(db);
 const middlewares = jsonServer.defaults();
+
+const authConfig = {
+    tokenExpiration: '48h' // 修改註冊帳號及密碼保留的時間為1小時
+  };
+  
+server.use(jsonServer.auth(authConfig));
 server.use(cors())
 server.use(middlewares)
 server.db = router.db;
