8e2d6a5bb15a1f1af83bf8dd69a14e914a3d5834
facina <lovemistborn13@gmail.com>
2023-02-22 17:59:28 +0800

    try require

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const db = require("./db.json");
 const router = jsonServer.router(db);
 const middlewares = jsonServer.defaults();
 
-const authConfig = {
+const authConfig = require({
     tokenExpiration: '48h' // 修改註冊帳號及密碼保留的時間為1小時
-  };
+  });
   
 server.use(authConfig);
 server.use(cors())
